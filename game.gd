76a68f8bbf7d650e2aa6e83956f0cda3de6efb70
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2026-08-21 16:05:48 -0500

    sprint

--- game.gd
+++ game.gd
@@ -11,7 +11,7 @@ func _ready() -> void:
 
 
 func _process(_delta: float) -> void:
-	var actions := ["move_left", "move_right", "jump"]
+	var actions := ["move_left", "move_right", "jump", "sprint"]
 
 	var pressed_actions: Array = actions.filter(
 		func(input) -> bool:
--- player.gd
+++ player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-
-const MAX_SPEED = 700.0
+const DEFAULT_SPEED = 400.0
+const SPRINT_SPEED = 750.0
 const JUMP_VELOCITY = -1200.0
 const GRAVITY = 1700.0
 const MOVE_ACCELERATION = 1500.0
@@ -39,55 +39,60 @@ func _physics_process(delta: float) -> void:
 			_move(delta)
 			_process_buffered_jump(delta)
 			_check_fallout(delta)
-			
-			
+
 		State.JUMPING:
 			_move(delta)
 			_check_fallout(delta)
-			
+
 		State.FALLOUT:
 			pass
 
 
-func _process_buffered_jump(delta:float)->void:
+func _process_buffered_jump(delta: float) -> void:
 	if jump_buffer > 0:
 		jump_buffer -= delta
-	
+
 	if is_on_floor() and jump_buffer > 0:
 		jump_buffer = 0
-	
+
 		state = State.JUMPING
-				
+
 		var tween := create_tween()
 		tween.tween_property(self, 'velocity:y', 0, 0.3).from(JUMP_VELOCITY)
 		await tween.finished
-		
+
 		state = State.DEFAULT
 
-func _apply_gravity(delta:float) -> void:
+
+func _apply_gravity(delta: float) -> void:
 	if not is_on_floor() and state != State.JUMPING:
 		velocity += Vector2.DOWN * GRAVITY * delta
-		
-		
-func _move(delta:float) -> void:
+
+
+func _move(delta: float) -> void:
 	var direction := Input.get_axis("move_left", "move_right")
-	if direction != 0:
-		var accel := (
-			TURNAROUND_ACCELERATION
-			if signf(direction) != signf(velocity.x) else
-			MOVE_ACCELERATION
-		)
-		velocity.x = move_toward(velocity.x, MAX_SPEED * direction, accel * delta)
-	else:
+
+	if direction == 0:
 		var friction := GROUND_FRICTION if is_on_floor() else AIR_FRICTION
 		velocity.x = move_toward(velocity.x, 0, friction * delta)
+	else:
+		var speed := (SPRINT_SPEED
+			if Input.is_action_pressed("sprint")
+			else DEFAULT_SPEED)
+
+		var accel := (
+			TURNAROUND_ACCELERATION
+			if signf(direction) != signf(velocity.x)
+			else MOVE_ACCELERATION
+		)
+		velocity.x = move_toward(velocity.x, speed * direction, accel * delta)
 
 	move_and_slide()
-	
-	
-func _check_fallout(delta:float) -> void:
+
+
+func _check_fallout(_delta: float) -> void:
 	if global_position.y > 1000:
-		global_position = Vector2(100,100)
+		global_position = Vector2(100, 100)
 		velocity = Vector2()
 		state = State.FALLOUT
 		fallout.emit()
