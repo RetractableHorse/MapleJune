ee0f90e9098caad5989a90f472755a1c781777a9
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2026-08-26 15:45:34 -0500

    wall slide/jump

--- entities/player.gd
+++ entities/player.gd
@@ -1,6 +1,7 @@
 class_name Player
 extends CharacterBody2D
 
+@export_category("Walking & Running")
 @export_range(1.0, 5000.0) var max_speed := 325.0
 @export_range(1.0, 5000.0) var move_acceleration := 750.0
 
@@ -11,11 +12,12 @@ extends CharacterBody2D
 ## How fast you slow down on the ground
 @export_range(1.0, 5000.0) var ground_friction := 500.0
 
+
+@export_category("Jumping")
 ## How fast you slow down in the air
 @export_range(1.0, 5000.0) var air_friction := 50.0
 
 @export_range(1.0, 5000.0) var jump_speed := 440.0
-@export_range(1.0, 5000.0) var dash_jump_speed := 500.0
 @export_range(1.0, 5000.0) var gravity := 1800.0
 @export_range(0.0, 1.0, 0.01) var max_jump_time := 0.2
 
@@ -25,22 +27,31 @@ extends CharacterBody2D
 ## Grace period for when the player can jump right before hitting the ground
 @export_range(0.0, 1.0, 0.01) var max_trampoline_time := 0.08
 
+
+@export_category("Dashing")
 @export_range(1.0, 5000.0) var dash_speed := 500.0
-@export_range(0.0, 5.0, 0.05) var max_dash_time := 0.4
-@export var infinite_dash := false
 @export_range(1.0, 5000.0) var dash_air_impulse := 150.0
 @export_range(1.0, 5000.0) var dash_gravity := 300.0
+@export_range(1.0, 5000.0) var dash_jump_speed := 500.0
+@export var infinite_dash := false
+@export_range(0.0, 5.0, 0.05) var max_dash_time := 0.4
 
 signal state_changed(state: State)
 
 enum State {
 	IDLE,
 	FALLING,
-	WALKING,
 	RUNNING,
 	JUMPING,
 	DASHING,
+	WALL_SLIDE,
 	DEAD,
+}
+
+## for values that should only ever be -1 or 1, never 0
+enum Direction {
+	LEFT = -1,
+	RIGHT = 1,
 }
 
 var state := State.IDLE:
@@ -49,18 +60,25 @@ var state := State.IDLE:
 		state_changed.emit(new_state)
 
 var movement := 0.0
-var facing := 1
-var dash_facing := 1
-var remaining_dash_time := 0.0
+var facing := Direction.RIGHT
+
+var jump_requested := false
 var remaining_jump_time := 0.0
 var remaining_coyote_time := 0.0
 var remaining_trampoline_time := 0.0
+
+var dash_facing := Direction.RIGHT
+var remaining_dash_time := 0.0
 
 @onready var sprite_container: Node2D = $SpriteContainer
 
 var is_dashing: bool:
 	get:
 		return remaining_dash_time > 0.0
+
+var is_wall_sliding: bool:
+	get:
+		return is_on_wall_only() and signf(get_wall_normal().x) == -signf(movement)
 
 
 func reset() -> void:
@@ -84,7 +102,7 @@ func _process(_delta: float) -> void:
 		_finish_dash()
 
 	if movement != 0:
-		facing = int(signf(movement))
+		facing = int(signf(movement)) as Direction
 
 	sprite_container.scale.x = facing
 
@@ -100,76 +118,20 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 
-func _process_state(delta: float) -> void:
-	match state:
-		State.IDLE:
-			_apply_ground_friction(delta)
+func _process_jump(delta: float) -> void:
+	remaining_jump_time -= delta
+	remaining_trampoline_time -= delta
+	remaining_coyote_time -= delta
 
-			velocity.y = 0
+	if is_on_floor() and (jump_requested or remaining_trampoline_time > 0):
+		remaining_jump_time = max_jump_time
+		if is_wall_sliding:
+			velocity.x = get_wall_normal().x * 300
 
-			if _check_falling():
-				return
+	if jump_requested and not is_on_floor():
+		remaining_trampoline_time = max_trampoline_time
 
-			if is_on_floor() and movement != 0:
-				state = State.WALKING
-
-		State.FALLING:
-			_apply_movement_accel(delta)
-			_apply_gravity(delta)
-
-			remaining_coyote_time -= delta
-			remaining_trampoline_time -= delta
-
-			if is_on_floor():
-				if remaining_trampoline_time > 0:
-					state = State.JUMPING
-					remaining_jump_time = max_jump_time
-				else:
-					state = State.IDLE
-					velocity.y = 0
-
-		State.WALKING:
-			_apply_movement_accel(delta)
-
-			if not is_on_floor():
-				state = State.FALLING
-
-			if movement == 0:
-				state = State.IDLE
-
-		State.RUNNING:
-			pass
-
-		State.JUMPING:
-			_apply_movement_accel(delta)
-
-			var applied_jump_speed = jump_speed
-
-			if is_dashing:
-				applied_jump_speed = dash_jump_speed
-
-			velocity.y = -applied_jump_speed
-			remaining_jump_time -= delta
-
-			if remaining_jump_time < 0:
-				state = State.FALLING
-
-		State.DASHING:
-			velocity.x = dash_facing * dash_speed
-
-			# while dashing, if we're moving upward, apply lessened gravity to move downward,
-			# but once we're level, stay level
-			velocity.y = minf(velocity.y + dash_gravity * delta, 0)
-
-			if infinite_dash:
-				return
-
-			remaining_dash_time -= delta
-			if remaining_dash_time < 0:
-				if is_on_floor():
-					state = State.IDLE
-				else:
-					state = State.FALLING
+	jump_requested = false
 
 
 func _apply_gravity(delta: float) -> void:
@@ -178,6 +140,10 @@ func _apply_gravity(delta: float) -> void:
 
 func _apply_ground_friction(delta: float) -> void:
 	velocity.x = move_toward(velocity.x, 0, ground_friction * delta)
+
+
+func _apply_air_friction(delta: float) -> void:
+	velocity.x = move_toward(velocity.x, 0, air_friction * delta)
 
 
 func _apply_movement_accel(delta: float) -> void:
@@ -190,6 +156,26 @@ func _check_falling() -> bool:
 
 	state = State.FALLING
 	remaining_coyote_time = max_coyote_time
+
+	return true
+
+
+func _check_jump() -> bool:
+	var should_jump := is_on_floor() and remaining_trampoline_time > 0
+	if not should_jump:
+		return false
+
+	state = State.JUMPING
+	remaining_jump_time = max_jump_time
+
+	return true
+
+
+func _check_wall_slide() -> bool:
+	if not is_on_wall_only():
+		return false
+
+	state = State.WALL_SLIDE
 	return true
 
 
@@ -226,15 +212,13 @@ func _update_movement_velocity(delta: float):
 
 
 func _start_jump():
-	var is_falling_with_remaining_coyote_time := (
-		state == State.FALLING and remaining_coyote_time > 0
-	)
-
-	var can_jump = is_on_floor() or is_falling_with_remaining_coyote_time
-
-	if can_jump:
+	if (is_on_floor() or (state == State.FALLING and remaining_coyote_time > 0)):
 		state = State.JUMPING
 		remaining_jump_time = max_jump_time
+	elif state == State.WALL_SLIDE:
+		state = State.JUMPING
+		remaining_jump_time = max_jump_time
+		velocity.x = get_wall_normal().x * 300.0
 	else:
 		# if we can't jump, reset the remaining trampoline time,
 		# so a jump can happen later if we hit the ground before it reaches 0
@@ -259,6 +243,106 @@ func _finish_dash() -> void:
 	if not is_on_floor():
 		state = State.FALLING
 	elif movement != 0:
-		state = State.WALKING
+		state = State.RUNNING
 	else:
 		state = State.IDLE
+
+
+func _process_state(delta: float) -> void:
+	match state:
+		State.IDLE:
+			_apply_ground_friction(delta)
+
+			velocity.y = 0
+
+			if _check_falling():
+				return
+
+			if is_on_floor() and movement != 0:
+				state = State.RUNNING
+
+		State.RUNNING:
+			_apply_gravity(delta)
+
+			if movement == 0:
+				_apply_ground_friction(delta)
+			else:
+				_apply_movement_accel(delta)
+
+			if _check_wall_slide():
+				return
+
+			if is_on_floor() and movement == 0:
+				state = State.IDLE
+
+			if _check_falling():
+				return
+
+		State.FALLING:
+			_apply_gravity(delta)
+
+			remaining_trampoline_time -= delta
+			remaining_coyote_time -= delta
+
+			if movement == 0:
+				_apply_air_friction(delta)
+			else:
+				_apply_movement_accel(delta)
+
+			if is_on_floor():
+				if remaining_trampoline_time > 0:
+					state = State.JUMPING
+					remaining_jump_time = max_jump_time
+				elif movement != 0:
+					state = State.RUNNING
+				else:
+					state = State.IDLE
+				return
+
+			if _check_wall_slide():
+				return
+
+		State.JUMPING:
+			_apply_movement_accel(delta)
+			velocity.y = -jump_speed
+
+			remaining_jump_time -= delta
+
+			if remaining_jump_time < 0:
+				state = State.FALLING
+
+			if _check_wall_slide():
+				return
+
+		State.WALL_SLIDE:
+			velocity.y = minf(velocity.y + 1000.0 * delta, 150.0)
+
+			if is_on_floor():
+				state = State.IDLE
+				return
+
+			if not is_on_wall():
+				if is_on_floor():
+					state = State.IDLE
+				else:
+					state = State.FALLING
+
+		State.DASHING:
+			if _check_wall_slide():
+				return
+
+			velocity.x = dash_facing * dash_speed
+
+			# while dashing, if we're moving upward, apply lessened gravity to move downward,
+			# but once we're level, stay level
+			velocity.y = minf(velocity.y + dash_gravity * delta, 0)
+
+			if infinite_dash:
+				return
+
+			remaining_dash_time -= delta
+			if remaining_dash_time < 0:
+				if is_on_floor():
+					state = State.IDLE
+				else:
+					state = State.FALLING
